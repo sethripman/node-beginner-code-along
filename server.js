f1c1ed1ce9aff0139710eb40b1a48843117b7c95
sethripman <sethripman@gmail.com>
2019-12-08 23:50:19 -0800

    response handlers

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 let http = require('http');
 let url = require('url');
 
-function start() {
+function start(route) {
   function onRequest(request, response) {
     let pathname = url.parse(request.url).pathname;
     console.log(`Request for ${pathname} received`);
 
     route(pathname);
-    
+
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Hello World!');
     response.end();
