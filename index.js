162246837e498d6231b09618d68c09cda0449c02
sethripman <sethripman@gmail.com>
2019-12-08 23:55:53 -0800

    working

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let server = require('./server');
 let router = require('./router');
+let requestHandlers = require('./requestHandlers');
 
+let handle = {};
 
-server.start(router.route);
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+
+server.start(router.route, handle);
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 let http = require('http');
 let url = require('url');
 
-function start(route) {
+function start(route, handle) {
   function onRequest(request, response) {
     let pathname = url.parse(request.url).pathname;
     console.log(`Request for ${pathname} received`);
 
-    route(pathname);
+    route(handle, pathname);
 
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Hello World!');
