45c1f5c3ae6b8e1e35461c4baf22dc5cb34ebd90
sethripman <sethripman@gmail.com>
2019-12-09 00:11:02 -0800

    fixing request handlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,14 +1,13 @@
 let exec = require('child_process').exec;
 
-function start() {
+function start(response) {
   console.log(`Request handler ${start} was called.`);
-  let content = 'empty';
 
   exec('ls -lah', function(error, stdout, stderr) {
-    content = stdout;
+    response.writeHead(200, { 'Content-Type':'text/plain' });
+    response.write(stdout);
+    response.end();
   });
-      
-  return content;
 }
     
 function upload() {
