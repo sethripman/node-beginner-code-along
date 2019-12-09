38ddcae64e86d36e56a94dd7d1c942415a91b26e
sethripman <sethripman@gmail.com>
2019-12-09 00:12:33 -0800

    bleh

--- requestHandlers.js
+++ requestHandlers.js
@@ -10,9 +10,11 @@ function start(response) {
   });
 }
     
-function upload() {
+function upload(response) {
   console.log(`Request handler ${upload} was called.`);
-  return 'hello upload';
+  response.writeHead(200, { 'Content-Type':'text/plain' });
+  response.write('Hello Upload');
+  response.end();
 }
     
 exports.start = start;
