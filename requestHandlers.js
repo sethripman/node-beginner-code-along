50a8f45135515483d87409a03ded4c92a3adab75
sethripman <sethripman@gmail.com>
2019-12-09 00:04:16 -0800

    working

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,19 @@
+let exec = require('child_process').exec;
+
 function start() {
   console.log(`Request handler ${start} was called.`);
+  let content = 'empty';
+
+  exec('ls -lah', function(error, stdout, stderr) {
+    content = stdout;
+  });
+      
+  return content;
 }
     
 function upload() {
   console.log(`Request handler ${upload} was called.`);
+  return 'hello upload';
 }
     
 exports.start = start;
